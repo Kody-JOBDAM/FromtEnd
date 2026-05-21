8d22c3c8340fd2137672c13d269843090fd82d29
soonwoo <sunwoosunwoo4114@gmail.com>
2026-05-21 17:05:09 +0900

    feat: implement login page UI

--- app/(auth)/forgot-password/page.tsx
+++ app/(auth)/forgot-password/page.tsx
@@ -1,14 +1,14 @@
-/* 회원가입 페이지 */
+//비밀번호 재설정 페이지
 "use client";
 
 import { useRouter } from "next/navigation";
 
-export default function SignupPage() {
+export default function ForgotPasswordPage() {
   const router = useRouter();
 
   return (
     <main className="flex flex-col items-center justify-center min-h-screen">
-      <p>회원가입 페이지</p>
+      <p>비밀번호 찾기 페이지</p>
       <text
         className="text-[#02C551] cursor-pointer"
         onClick={() => router.push("/login")}
